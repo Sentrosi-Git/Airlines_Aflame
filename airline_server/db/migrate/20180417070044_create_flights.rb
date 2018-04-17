d2458910f1d9f4b061ed9e272d516d42cecb1041
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-04-18 06:56:49 +1000

    added columns to flights migration file

--- airline_server/db/migrate/20180417070044_create_flights.rb
+++ airline_server/db/migrate/20180417070044_create_flights.rb
@@ -1,7 +1,12 @@
 class CreateFlights < ActiveRecord::Migration[5.1]
   def change
     create_table :flights do |t|
-
+      t.text :flight_number
+      t.text :origin
+      t.text :destination
+      t.datetime :departure_date
+      t.datetime :arrival_date
+      
       t.timestamps
     end
   end
--- airline_server/db/migrate/20180417070108_create_planes.rb
+++ airline_server/db/migrate/20180417070108_create_planes.rb
@@ -1,7 +1,7 @@
 class CreatePlanes < ActiveRecord::Migration[5.1]
   def change
     create_table :planes do |t|
-
+      
       t.timestamps
     end
   end
