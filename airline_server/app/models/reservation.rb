c8f263f32c85ec27dcfa9941491b8a734c78d90d
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-04-18 13:51:15 +1000

    created visualization of plane rows and columns

--- airline_server/app/models/reservation.rb
+++ airline_server/app/models/reservation.rb
@@ -1,4 +1,22 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :flight
+
+  private
+  def seat_limits
+    unless flight_id == nil
+      row = seat[0].ord - 64
+      col = seat[1..seat.size].to_i
+      plane_rows = self.flight.plane.rows
+      plane_cols = self.flight.plane.cols
+
+      if row > plane_rows
+        errors.add(:seat, "row doesn't exist. Please capitalize the letter or check the number of rows on the plane for this flight.")
+      elsif col < 1
+        errors.add(:seat, "column doesn't exist.")
+      elsif col > plane_cols
+        errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
+      end
+    end
+  end
 end
