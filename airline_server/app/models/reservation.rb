695ffd2e09e6556ae31612269e2da6b91836c9d3
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-04-18 16:33:57 +1000

    fixed seat double book conflict

--- airline_server/app/models/reservation.rb
+++ airline_server/app/models/reservation.rb
@@ -1,20 +1,25 @@
+
 class Reservation < ApplicationRecord
-  belongs_to :user
   belongs_to :flight
+  belongs_to :user
+
+  validates :seat, :presence => true, format: { with: /\A[A-Z]\d+\z/, message: "format incorrect. Please enter seat number in the correct format, e.g. A1."}
+  validates_uniqueness_of :seat, :scope => [:flight_id] # checks if seat is taken
+  validate :seat_limits
 
   private
   def seat_limits
     unless flight_id == nil
       row = seat[0].ord - 64
-      col = seat[1..seat.size].to_i
+      column = seat[1..seat.size].to_i
       plane_rows = self.flight.plane.rows
-      plane_cols = self.flight.plane.cols
+      plane_columns = self.flight.plane.columns
 
       if row > plane_rows
         errors.add(:seat, "row doesn't exist. Please capitalize the letter or check the number of rows on the plane for this flight.")
-      elsif col < 1
+      elsif column < 1
         errors.add(:seat, "column doesn't exist.")
-      elsif col > plane_cols
+      elsif column > plane_columns
         errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
       end
     end
